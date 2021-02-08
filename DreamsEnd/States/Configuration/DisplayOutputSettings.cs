9cbef5923e22f97e1338c9e73d1725ae46b70dcd
Thomas Andree Wang <thomas.a.wang@outlook.com>
2021-02-08 13:48:10 +0100

    Fixed display size

--- DreamsEnd/States/Configuration/DisplayOutputSettings.cs
+++ DreamsEnd/States/Configuration/DisplayOutputSettings.cs
@@ -4,8 +4,9 @@ namespace DreamsEnd.States.Configuration
 {
     public static class DisplayOutputSettings
     {
+        const int BaseHeigth = 1200;
+        const int BaseWidth = 1920;
         internal static bool isFullScreen = false;
-
 
         private static DisplayMode displayMode;
         internal static DisplayMode DisplayMode
@@ -14,14 +15,14 @@ namespace DreamsEnd.States.Configuration
             set
             {
                 displayMode = value;
-                ScreenHeight = value.Height;
-                ScreenWidth = 2560; //value.Width;
+                ScreenHeight = isFullScreen ? value.Height : BaseHeigth;
+                ScreenWidth = isFullScreen ? value.Width : BaseWidth;
             }
         }
 
         public static int CenterScreenTileOffsetX, CenterScreenTileOffsetY;
 
-        private static int _screenHeight = 640;
+        private static int _screenHeight = DisplayMode?.Height ?? BaseHeigth;
         public static int ScreenHeight
         {
             get
@@ -31,11 +32,11 @@ namespace DreamsEnd.States.Configuration
             private set
             {
                 _screenHeight = value;
-                CenterScreenTileOffsetY = (int)((value / 2560f) * (2 * EngineSettings.TileSize));
+                CenterScreenTileOffsetY = (int)((value / (double)DisplayMode.Width) * (2 * EngineSettings.TileSize));
             }
         }
 
-        private static int _screenWidth = 480;
+        private static int _screenWidth = DisplayMode?.Width ?? BaseWidth;
         public static int ScreenWidth
         {
             get
@@ -45,7 +46,7 @@ namespace DreamsEnd.States.Configuration
             private set
             {
                 _screenWidth = value;
-                CenterScreenTileOffsetX = (int)((value / 1440f) * (2 * EngineSettings.TileSize));
+                CenterScreenTileOffsetX = (int)((value / (double)DisplayMode.Height) * (2 * EngineSettings.TileSize));
             }
         }
     }
